940cd6dacd9e7078ff3710581af68a3d1de97420
Tom Faulhaber <git_net@infolace.com>
2011-07-18 23:56:45 -0700

    no change

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject indent "1.0.0"
   :description "A sample for how to modify the simple dispatch in pprint with larger indents"
   :url "http://github.com/tomfaulhaber/pprint-indent"
-  :main indent.indent
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]])
